051c7347996f1f9b689f3cb29bbb6ab8843949b0
Anton Tikhomirov <acuna.public@gmail.com>
2024-08-03 17:02:42 +0300

    Update Exchange.php

--- Exchange.php
+++ Exchange.php
@@ -498,15 +498,21 @@
 		
 		final function close () {
 			
-			if ($this->liquidPrice > 0) if (
-				($this->isLong () and $this->price['low'] <= $this->liquidPrice) or
-				($this->isShort () and $this->price['high'] >= $this->liquidPrice)
-			) {
+			if ($this->liquidPrice > 0) {
 				
-				$this->pnl = -$this->margin;
-				$this->roi = $this->roe = -100; // TODO
+				if (
+					($this->isLong () and $this->price['low'] <= $this->liquidPrice) or
+					($this->isShort () and $this->price['high'] >= $this->liquidPrice)
+				) {
+					
+					$this->pnl = -$this->margin;
+					$this->roi = $this->roe = -100; // TODO
+					
+				}
 				
-			} elseif ($this->pnl > 0 and $this->withdrawPercent > 0) {
+			}
+			
+			if ($this->pnl > 0 and $this->withdrawPercent > 0) {
 				
 				$percent = new \Percent ($this->pnl);
 				
